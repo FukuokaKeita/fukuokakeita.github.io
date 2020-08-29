819cce4d9637c7c2685e8079a5b90c2a98226573
kingdream <lvsoccer@icloud.com>
2020-08-29 15:45:25 +0900

    error correction

--- javascript/js.js
+++ javascript/js.js
@@ -1,35 +1,36 @@
-//■page topボタン
 
-$(function(){
-    var topBtn=$('#pageTop');
-    topBtn.hide();
-    
-     
-    
-    //◇ボタンの表示設定
-    $(window).scroll(function(){
-      if($(this).scrollTop()>80){
-    
-        //---- 画面を80pxスクロールしたら、ボタンを表示する
-        topBtn.fadeIn();
-    
-      }else{
-    
-        //---- 画面が80pxより上なら、ボタンを表示しない
-        topBtn.fadeOut();
-    
-      }
-    });
-    
-     
-    
-    // ◇ボタンをクリックしたら、スクロールして上に戻る
-    topBtn.click(function(){
-      $('body,html').animate({
-      scrollTop: 0},500);
-      return false;
-    
-    });
-    
-    
-    });
+// PhotoSwipe
+initPhotoSwipeFromDOM('.my-gallery');
+
+$(function () {
+
+  $('.card-caption').on('click', 'a', function (e) {
+    e.stopPropagation();
+  });
+
+  var $nav = $('#gnav');
+  var offset = $nav.offset();
+  var navHeight = $nav.innerHeight();
+  var headerInner = $('#header .inner');
+
+  //ページ内スクロール
+  $('a[href^="#"]').on('click', function () {
+    var speed = 300;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({
+      scrollTop: position
+    }, speed, "swing");
+    return false;
+  });
+
+  //ページトップへもどる
+  $('#js-pageTop').on('click', function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 300);
+    return false;
+  });
+
+});
